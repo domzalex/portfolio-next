d74829fab745702597c58b11cb16094d4e8f93f8
Alex Domzalski <alexdomzalski@gmail.com>
2024-11-07 11:17:52 -0500

    bug bandaids

--- src/app/components/desktop_icons.tsx
+++ src/app/components/desktop_icons.tsx
@@ -66,24 +66,20 @@ const DesktopIcons = ({windowsOpen, setWindowsOpen, mainHeight, windowRef}: Prop
             {name: 'Blog', image: blogImage, icon: internet, component: {component: Blog, props: {sentUrl: 'https://theculdesac.club/blog'}}},
         ])
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [windowRef])
+    }, [])
     const [icons, setIcons] = useState<Icon[]>([
     ])
 
     return (
         <div className="w-full flex flex-col gap-3 flex-wrap absolute top-0 left-0 p-1">
-            {windowRef ? (
-                icons.map((icon, index) => {
-                    return (
-                        <div key={index} onClick={() => setWindowsOpen((prev) => [...prev, {id: Math.random().toString(36).substr(2,9), name: icon.name, size: {width:850, height: mainHeight}, setSize: {width:720, height: 480}, prevPos: {top: windowRef && windowRef.getBoundingClientRect().width <= 640 ? 0 : (100 + (windowsOpen.length * 40)), left:  windowRef && windowRef.getBoundingClientRect().width <= 640 ? 0 : (250 + (windowsOpen.length * 40))}, pos: {top:  windowRef && windowRef.getBoundingClientRect().width <= 640 ? 0 : (100 + (windowsOpen.length * 40)), left:  windowRef && windowRef.getBoundingClientRect().width <= 640 ? 0 : (250 + (windowsOpen.length * 40))}, icon: icon.icon, component: icon.component}])} className="hover:border-white/40 hover:bg-cyan-600/30 active:border-white/20 active:bg-cyan-700/30 w-1/5 rounded max-w-[85px] border border-transparent flex flex-col items-center justify-center gap-1 py-2">
-                            <Image src={icon.image} alt={icon.name} width={50} height={50} className="select-none"/>
-                            <p className="select-none text-white text-xs pointer-events-none" style={{ textShadow: '1px 1px 1px #000' }}>{icon.name}</p>
-                        </div>
-                    )
-                })
-            ) : (
-                <></>
-            )}
+            {icons.map((icon, index) => {
+                return (
+                    <div key={index} onClick={() => setWindowsOpen((prev) => [...prev, {id: Math.random().toString(36).substr(2,9), name: icon.name, size: {width:850, height: mainHeight}, setSize: {width:720, height: 480}, prevPos: {top: windowRef && windowRef.getBoundingClientRect().width <= 640 ? 0 : (100 + (windowsOpen.length * 40)), left:  windowRef && windowRef.getBoundingClientRect().width <= 640 ? 0 : (250 + (windowsOpen.length * 40))}, pos: {top:  windowRef && windowRef.getBoundingClientRect().width <= 640 ? 0 : (100 + (windowsOpen.length * 40)), left:  windowRef && windowRef.getBoundingClientRect().width <= 640 ? 0 : (250 + (windowsOpen.length * 40))}, icon: icon.icon, component: icon.component}])} className="hover:border-white/40 hover:bg-cyan-600/30 active:border-white/20 active:bg-cyan-700/30 w-1/5 rounded max-w-[85px] border border-transparent flex flex-col items-center justify-center gap-1 py-2">
+                        <Image src={icon.image} alt={icon.name} width={50} height={50} className="select-none"/>
+                        <p className="select-none text-white text-xs pointer-events-none" style={{ textShadow: '1px 1px 1px #000' }}>{icon.name}</p>
+                    </div>
+                )
+            })}
             <a href="https://github.com/domzalex" target="blank" rel="noopener noreferrer" className="hover:border-white/40 hover:bg-cyan-600/30 active:border-white/20 active:bg-cyan-700/30 w-1/5 rounded max-w-[85px] border border-transparent flex flex-col items-center justify-center gap-1 py-2">
                 <Image src={github} alt="github link" width={50} height={50} className="select-none"/>
                 <p className="select-none text-white text-xs pointer-events-none" style={{ textShadow: '1px 1px 1px #000' }}>GitHub</p>
